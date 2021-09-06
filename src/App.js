fb4fc63e5c2dc39c5e1966dd48f4050309b47961
NelinhoHill <nelison.doidao@gmail.com>
2021-09-05 23:01:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
 import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-
-
-import CalculoIMC from './componentes/CalculoIMC';
-
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={estilos.container} >
-        <CalculoIMC />
-      </View>
-    );
-  }
-}
+import { NavigationContainer } from '@react-navigation/native';
+import { MainStackNavigator } from './StackNavigator';
+import BottomTabNavigator from "./TabNavigator";
+import DrawerNavigator from './DrawerNavigator';
 
 
 
-// export default App = () => {
-//   return (
-//     <NavigationContainer>
-//       <DrawerNavigator  />
-//     </NavigationContainer>
-//   );
-// }
+ export default App = () => {
+   return (
+     <NavigationContainer>
+      <DrawerNavigator  />
+     </NavigationContainer>
+   );
+ }
 
 
 
-const estilos = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+//const estilos = StyleSheet.create({
+ // container: {
+   // flex: 1,
+    //justifyContent: 'center',
+    //alignItems: 'center',
+//},
 
-  fonte: {
-    fontSize: 40,
-  },
-});
+//fonte: {
+ //  fontSize: 40,
+ //},
+ //}
